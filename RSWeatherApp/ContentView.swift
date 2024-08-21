2c3a3d2d8ebea9a79854df33846f5ebcbee2d6ef
Ricardo Silva <ricardosilva777@gmail.com>
2024-08-21 23:35:28 +0100

    chore: add SwiftLint package
    Fix warnings detected.

--- RSWeatherApp/ContentView.swift
+++ RSWeatherApp/ContentView.swift
@@ -1,10 +1,3 @@
-//
-//  ContentView.swift
-//  RSWeatherApp
-//
-//  Created by Ricardo Silva on 21/08/2024.
-//
-
 import SwiftUI
 import CoreData
 
@@ -51,7 +44,8 @@ struct ContentView: View {
                 try viewContext.save()
             } catch {
                 // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                // fatalError() causes the application to generate a crash log and terminate. 
+                // You should not use this function in a shipping application.
                 let nsError = error as NSError
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
@@ -66,7 +60,8 @@ struct ContentView: View {
                 try viewContext.save()
             } catch {
                 // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                // fatalError() causes the application to generate a crash log and terminate. 
+                // You should not use this function in a shipping application.
                 let nsError = error as NSError
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
